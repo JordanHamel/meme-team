9d3aa66fad7ef94e54b9abf07b887952b3819587
Jordan+Mario <Jordan+Mario@appacademy.io>
2013-02-14 15:55:26 -0800

    clickController and face box done

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+function makeClicksController(element) {
+  var clicksController = {
+    canvas: null,
+
+    bind: function (canvas) {
+      this.canvas = $(canvas);
+      // add click event trigger
+      this.canvas.click(this.doClick.bind(this));
+    },
+
+    doClick: function (event) {
+      // Get the coord of where the click event took
+      // place
+      var coords = {
+        x: event.pageX,
+        y: event.pageY
+      };
+
+      // Pop a face box @ the event's coords
+      this.addFaceBox(coords);
+    },
+
+    addFaceBox: function (coords) {
+      this.canvas.append(
+        $("<div></div>")
+          .addClass("box")
+          .css("left", coords.x)
+          .css("top", coords.y)
+      )
+    }
+  }
+
+  clicksController.bind(element);
+
+  return clicksController;
+};
+
+
 $(function() {
-
-
-
-
-
-
-
+ // any stuff that manipulates the dom
+   makeClicksController($("#canvas"));
 });
