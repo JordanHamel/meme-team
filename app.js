19cd3cced50342c96a3e540717119e8a6c28606a
Jordan+Mario <Jordan+Mario@appacademy.io>
2013-02-15 11:55:37 -0800

    Refactored the way the canvas is displayed and wrote first pass at showing a names list

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function makeClicksController(element) {
     canvas: null,
 
     bind: function (canvas) {
-      this.canvas = $(canvas);
+      this.canvas = canvas;
       // add click event trigger
       this.canvas.click(this.doClick.bind(this));
     },
@@ -11,14 +11,13 @@ function makeClicksController(element) {
     doClick: function (event) {
       // Get the coord of where the click event took place
       var coords = {
-        x: event.pageX,
-        y: event.pageY
+        x: event.pageX - $("#container").offset().left,
+        y: event.pageY - $("#container").offset().top
       };
 
       // Create a facebox object
-      var name = prompt("Who is this?");
-      var faceBox = makeFaceBox(coords, this.canvas, name);
-      faceBoxes.push(faceBox);
+      var name = ; //call list
+      makeFaceBox(coords, this.canvas, name);
     }
   }
 
@@ -36,6 +35,8 @@ function makeFaceBox(coords, canvas, name) {
     canvas: canvas
   };
 
+  faceBoxes.push(faceBox);
+
   return faceBox;
 };
 
@@ -52,7 +53,7 @@ function printFaceBox (facebox) {
   )
 }
 
-function toggleFaceBoxes(canvas){
+function toggleFaceBoxes(canvas) {
   if(canvas.is(':empty')){
     $(faceBoxes).each(function () {
       printFaceBox(this);
@@ -62,14 +63,44 @@ function toggleFaceBoxes(canvas){
   }
 }
 
-var faceBoxSeeds = [{
-    name: "Wolf",
-    coords: { x: 529, y: 452 },
-    canvas: $("#canvas")
-  }];
+function showNameList(names) {
+  var theDiv = $("#name-list");
+  var theUl = $("<ul></ul>");
+
+  for(var i=0; i<names.length; i++) {
+    theUl.append(
+      $("<li></li>").text(names[i])
+    )
+  }
+
+  theDiv.append(theUl);
+}
+
+function loadSeedData(seedData){
+  $.each(seedData, function(){
+    makeFaceBox(this.coords, this.canvas, this.name);
+  });
+}
+
+var names = ["Jordan", "Mario", "Sarah", "Flowers", "Puppies"];
 
 $(function() {
-   var canvas = $("#canvas");
-   makeClicksController(canvas);
-   $("#toggle").click(function(){ toggleFaceBoxes(canvas) });
+  var canvas = $("#canvas");
+  var faceBoxSeeds = [
+    {
+      name: "Wolf",
+      coords: { x: 307, y: 414 },
+      canvas: canvas
+    },
+    {
+      name: "WTF Dog",
+      coords: { x: 1064, y: 556 },
+      canvas: canvas
+    }
+  ];
+
+
+  makeClicksController(canvas);
+  $("#toggle").click(function(){ toggleFaceBoxes(canvas) });
+  $("#loadSeedData").click(function(){ loadSeedData(faceBoxSeeds) });
 });
